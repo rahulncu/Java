4a51f6ab2622e87e0b08011600643b6228d24ee1
rahulncu <54216964+rahulncu@users.noreply.github.com>
2021-09-21 14:59:35 +0530

    Add files via upload

---
+++ niit/src/niit/comer.java
@@ -0,0 +1,32 @@
+package niit;
+import java.util.*;
+
+public class comer {
+	public static void main(String arg[]) {
+		int spend, price, bought,free;
+		free=0;
+		
+		Scanner s=new Scanner(System.in);
+		
+		System.out.println("Enter the amount want to spend : ");
+		spend=s.nextInt();
+		
+		System.out.println("Enter price of an item : ");
+		price=s.nextInt();
+		
+		System.out.println("Enter the no. of items bought : ");
+		bought=s.nextInt();
+		
+		int itemstobuy=spend/price;
+		
+		if (itemstobuy>=10) {
+			free=itemstobuy/10;
+		}
+		
+		bought+=itemstobuy;
+		
+		System.out.println("Total number of items (free+purchased) : "+(free+itemstobuy));
+		
+	}
+
+}
---
+++ niit/src/niit/medi.java
@@ -0,0 +1,23 @@
+package niit;
+import java.util.*;
+
+public class medi {
+	public static void main(String arg[]) {
+		Scanner s=new Scanner(System.in);
+		
+		int today,gap;
+		String[] days = {"Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"};  
+		
+		System.out.println("Enter the number of the day today : ");
+		today=s.nextInt();
+		System.out.println("Enter the gap between medicines in days : ");
+		gap=s.nextInt();
+		
+		int nextday=((today+gap)%7)-1;
+		
+		System.out.println("Your next dose should be taken on : "+days[nextday]);
+		
+		
+	}
+
+}
